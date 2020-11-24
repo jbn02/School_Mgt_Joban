3f0d12b0df0003b28ce5d5560ed988bb373a2896
singhnirmal128@gmail.com <singhnirmal128@gmail.com>
2020-11-24 23:00:17 +0530

    Add project files.

---
+++ School_Mgt_Joban/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace School_Mgt_Joban.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
